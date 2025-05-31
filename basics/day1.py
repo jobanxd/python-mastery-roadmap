0da417f6454eb114f54a4bf9847c9d5ce68c0f91
jobanxd <sethjovan.pacturanan@gmail.com>
2025-05-31 10:22:14 +0800

    Day 1 done.

---
+++ basics/day1.py
@@ -0,0 +1,9 @@
+# Day 1 - Write a script that prints your name, age, and favorite programming language using variables.
+
+name = "jobanxd"
+age = "25"
+programming_language = "python"
+
+print(f"Username: {name}")
+print(f"Age: {age}")
+print(f"Current Programming Language: {programming_language}")
