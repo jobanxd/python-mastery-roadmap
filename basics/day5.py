2e58ee0ad98b87a2ef7dcbfdc3f5c58b461dbf50
jobanxd <sethjovan.pacturanan@gmail.com>
2025-06-04 19:29:35 +0800

    Day 5 Done

---
+++ basics/day5.py
@@ -0,0 +1,17 @@
+# - **Day 5:** Print the first 10 multiples of 3 using both `for` and `while` loops.
+
+# For loop
+print('This is For Loop Results:')
+for i in range(1,11):
+    if i%3 == 0:
+        print(i)
+
+print('This is While Loop Results:')
+i = 1 
+while i<=10:
+    if i%3 == 0:
+        print(i)
+    i += 1
+
+
+
